9e43de613d63060745a76584d0186475226f7081
Eddie Nakamoto <edwardnakamoto@icloud.com>
2024-06-06 16:56:47 -0500

    validator moved to .env from .ini

--- src/zangief/validator/config.py
+++ src/zangief/validator/config.py
@@ -1,15 +1,15 @@
-import configparser
-
+import os
+from dotenv import load_dotenv
 
 class Config:
-    def __init__(self, config_file):
-        if config_file is None:
-            config_file = "env/config.ini"
-        config = configparser.ConfigParser()
-        config.read(config_file)
+    def __init__(self, config_file=None, ignore_config=False):
+        # Load environment variables from the .env file if it exists. Does not override
+        # environment variables that are already set.
+        if (ignore_config == False):
+            load_dotenv(dotenv_path=config_file if config_file else 'env/.env', override=True)
+        
         self.validator = {
-            "name": config.get("validator", "name"),
-            "keyfile": config.get("validator", "keyfile"),
-            "interval": config.get("validator", "interval"),
-            "testnet": config.get("validator", "isTestnet"),
+            "keyfile": os.getenv("KEY_FILE"),
+            "interval": os.getenv("VALIDATOR_INTERVAL"),
+            "testnet": os.getenv("IS_TESTNET"),
         }
