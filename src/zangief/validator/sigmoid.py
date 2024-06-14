23684d15fefa5eba5d68f06a1f57c4ab916f608f
Eddie Nakamoto <edwardnakamoto@icloud.com>
2024-06-14 13:19:24 -0500

    new sigmoid

--- src/zangief/validator/sigmoid.py
+++ src/zangief/validator/sigmoid.py
@@ -26,26 +26,23 @@ def sigmoid_rewards(score_dict: Dict[int, float]) -> Dict[int, float]:
     # Calculate the mean score
     mean_score = sum(score_dict.values()) / len(score_dict)
 
-    # Set the threshold as a percentage above the mean score
-    threshold_percentage = 0.2  
-    threshold = mean_score * (1 + threshold_percentage)
+    # mean_score = mean(scores)
+    transformed_scores = []
+    for uid, score in score_dict.items():
+        if score > mean_score:
+            # Apply a power < 1 to scores above the mean to raise them, retaining separation
+            transformed_score = (score / mean_score) ** 0.9
+        else:
+            # Apply a power > 1 to scores below the mean to lower them, retaining separation
+            transformed_score = (score / mean_score) ** 1.1
 
-   
-    steepness = 20.0  # steepness for sharper punishment
+        # Ensure the transformed score is scaled back to between 0 and 1
+        transformed_scores.append(transformed_score)
 
-    # Set the high and low rewards
-    high_reward = 1.0
-    low_reward = 0.01  
+    for uid, t in zip(score_dict.keys(), transformed_scores):
+        score_dict[uid] = (t / max(transformed_scores))
 
-    # Calculate the adjusted scores using the sigmoid function
-    adjusted_scores : dict[int, float] = {}
-    for model_id, score in score_dict.items():
-        normalized_score = (score - threshold) * steepness
-        reward_ratio = sigmoid(normalized_score)
-        adjusted_score = low_reward + (high_reward - low_reward) * reward_ratio
-        adjusted_scores[model_id] = adjusted_score
-
-    return adjusted_scores
+    return score_dict
 
 
 # def sigmoid_rewards(score_dict: dict[int, float]) -> dict[int, float]:
