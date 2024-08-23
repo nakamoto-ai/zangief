93d4969d76ccbdefb88da26150f171b9fe0a14c7
rjmillerpy <rjmiller.py.dev@gmail.com>
2024-08-23 12:19:45 -0500

    feature extractor

--- src/zangief/miner/w2v2.py
+++ src/zangief/miner/w2v2.py
@@ -1,10 +1,10 @@
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from transformers import Wav2Vec2ForCTC, Wav2Vec2FeatureExtractor
 
 # Define the model name
 model_name = "facebook/wav2vec2-large-xlsr-53"
 
 # Download the processor and the model
-processor = Wav2Vec2Processor.from_pretrained(model_name)
+processor = Wav2Vec2FeatureExtractor.from_pretrained(model_name)
 # processor.save_pretrained("wav2vec2_model")
 model = Wav2Vec2ForCTC.from_pretrained(model_name)
 # model.save_pretrained("wav2vec2_model")
