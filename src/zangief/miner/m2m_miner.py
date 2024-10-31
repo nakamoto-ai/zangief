ae050b86b88cff53bda3dd02503b8aa0b1ec3ca1
rjmillerpy <rjmiller.py.dev@gmail.com>
2024-10-31 16:24:07 -0500

    fixed relative import

--- src/zangief/miner/m2m_miner.py
+++ src/zangief/miner/m2m_miner.py
@@ -2,7 +2,7 @@ from transformers import (
     M2M100ForConditionalGeneration,
     M2M100Tokenizer)
 from base_miner import BaseMiner
-from src.zangief.miner.config import Config
+from config import Config
 
 
 class M2MMiner(BaseMiner):
