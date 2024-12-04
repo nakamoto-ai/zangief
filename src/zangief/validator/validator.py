917498674bdfe8cd6493f8700b10a04b690aedc1
rjmillerpy <rjmiller.py.dev@gmail.com>
2024-12-04 16:47:52 -0600

    return scores fix hopefully

--- src/zangief/validator/validator.py
+++ src/zangief/validator/validator.py
@@ -338,9 +338,9 @@ class TranslateValidator(Module):
             miner_answers = [*it]
         return miner_answers
 
-    def return_miner_scores(self, full_scores: List[Dict[str, str]], miners_to_query: List[Dict[str, Any]]):
+    def return_miner_scores(self, full_scores: Dict[int, str], miners_to_query: List[Dict[str, Any]]):
         logger.info("Returning Miner scores...")
-        for i, full_score in enumerate(full_scores):
+        for i, full_score in enumerate(full_scores.values()):
             send_miner_score = partial(self._return_miner_scores, full_score)
             with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
                 logger.info(f"Full Score To Return: {full_score}")
